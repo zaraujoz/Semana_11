1e193d1a3ac0b614a534c60b6fc9a3a9cfd0ae1b
zaraujoz <matheusha@live.com>
2021-10-21 19:26:32 +0000

    Adicionando Arquivos

---
+++ Atributo.java
@@ -0,0 +1,9 @@
+public class Atributo {
+    
+  private  int numero;
+  public int numero2;
+  int numero3;
+  protected int numero4;
+
+
+}
---
+++ Programa.java
@@ -0,0 +1,24 @@
+public class Programa{
+    public static void main(String args[]){
+        int numero=10;
+                                                                                                                                    
+        Atributo2 at= new Atributo2();
+        Atributo2 at2= new Atributo2();
+        
+        at.numero2=10;
+        at2.numero2=20;
+        
+
+
+
+
+while(true){
+    int cont=0;
+    cont+=1;
+    if(cont>0){
+        break;
+    }
+System.out.println(cont);
+}
+}
+}
